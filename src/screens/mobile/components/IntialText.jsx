f9dd0233f9a92b85169c5c7207d270c93ab4fffa
unknown <automatizacionig@gmail.com>
2022-04-05 20:10:34 -0400

    changin img and adding triangle

--- src/screens/mobile/components/IntialText.jsx
+++ src/screens/mobile/components/IntialText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const IntialText = ({text, textClase}) => {
   return (
     <>
-    {/* <div className='triangle__text'></div> */}
+    <div className='triangle__text'></div>
     <div className='process__text-box'>
       <p className={textClase ? textClase :'process__main-text'}>{text}</p>
     </div>
